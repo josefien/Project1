2d6756c4bb2dd258312813de96b97d37c1cafa36
WimLogister <wimlogister@hotmail.com>
2016-01-08 21:37:50 +0100

    Working on scikit-svm, trying to make something that will make it easy to experiment with different kernels and different parameters.

--- svm/scikit-svm.py
+++ svm/scikit-svm.py
@@ -8,21 +8,29 @@ class Scikit-SVM:
 		self.kernel = kernel
 
 	def train(self,samples,responses):
-		X = self.kernel(X, params['gamma'])
+		X = self.kernel(X)
+		self.svm = SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
+    decision_function_shape='ovo', degree=3, gamma='auto', kernel='precomputed',
+    max_iter=-1, probability=False, random_state=None, shrinking=True,
+    tol=0.001, verbose=False)
 		self.svm = SVC(kernel='precomputed').fit(X,responses)
 
 	def predict(self,samples):
 		return 
 		pass
 
-	@staticmethod
-	def chi2(self,samples,gamma):
-		return chi2_kernel(samples,gamma)
-
-
+	""" Returns a chi-squared kernel where parameter gamma has been set
+		to parameter value.
+	"""
 	@staticmethod
 	def getChi2Kernel(self,gamma_val):
 		return functools.partial(chi2_kernel,gamma=gamma_val)
+
+	""" Returns a linear kernel
+	"""
+	@staticmethod
+	def getLinearKernel(self,gamma_val):
+		return functools.partial(linear_kernel)
 
 if __name__ == '__main__':
 	gammas=[]
@@ -30,4 +38,3 @@ if __name__ == '__main__':
 	kernels=[]
 	for gm in gammas:
 		kernels.append(getChi2Kernel(gm))
-	chi2_kernel = getChi2Kernel()
