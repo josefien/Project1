d4f7791c9af885c5757f7536b5a5966cc9659373
WimLogister <wimlogister@hotmail.com>
2016-01-09 16:20:27 +0100

    About ready to test the SVM. Right now we only have the mean accuracy of the trained classifier over all labels in the test set, because it's built into scikit, but we might have to look into others, such as precision and recall. However, these seem quite tricky to implement for the multi-class case. This has some formulas for multi-class performance measurement (page 4): http://atour.iro.umontreal.ca/rali/sites/default/files/publis/SokolovaLapalme-JIPM09.pdf

--- svm/scikit-svm.py
+++ svm/scikit-svm.py
@@ -1,5 +1,8 @@
 from sklearn.svm import SVC
 from sklearn.metrics.pairwise import chi2_kernel
+from sklearn.metrics.pairwise import linear_kernel
+from sklearn.metrics.pairwise import polynomial_kernel
+
 import functools
 import numpy as np
 import re
@@ -23,29 +26,37 @@ class Scikit_SVM:
 		return (X,y)
 
 	def train(self,samples,responses):
-		X = self.kernel(X)
-		self.svm = SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
-    decision_function_shape='ovo', degree=3, gamma='auto', kernel='precomputed',
-    max_iter=-1, probability=False, random_state=None, shrinking=True,
-    tol=0.001, verbose=False)
-		self.svm = SVC(kernel='precomputed').fit(X,responses)
+		self.svm = SVC(kernel=self.kernel).fit(samples,responses)
+		# Make sure we get the one-vs-rest style output that we want
+		self.svm.decision_function_shape = "ovr"
 
 	def predict(self,samples):
-		return 
-		pass
+		return np.float32( [self.svm.predict(s) for s in samples])
 
+	"""Return the mean accuracy of the trained classifier over all labels in the test set.
+		This is the only metric we have now, because it's built in, but we might have to look
+		into others, such as precision and recall, but these are quite tricky to implement
+		for the multi-class case. This paper has some formulas for multi-class performance
+		measurement (page 4): http://atour.iro.umontreal.ca/rali/sites/default/files/publis/SokolovaLapalme-JIPM09.pdf
+	"""
+	def score(self,test_samples,test_responses):
+		return self.svm.score(test_samples,test_responses)
 	""" Returns a chi-squared kernel where parameter gamma has been set
-		to parameter value.
+		to argument value.
 	"""
 	@staticmethod
 	def getChi2Kernel(self,gamma_val):
 		return functools.partial(chi2_kernel,gamma=gamma_val)
 
-	""" Returns a linear kernel
-	"""
+	"""Returns a linear kernel"""
 	@staticmethod
-	def getLinearKernel(self,gamma_val):
-		return functools.partial(linear_kernel)
+	def getLinearKernel(self):
+		return linear_kernel
+
+	"""Returns a polynomial kernel with parameters set to argument values"""
+	@staticmethod
+	def getPolyKernel(self,coef0,degree):
+		return functools.partial(polynomial_kernel,coef0=coef0,degree=degree)
 
 # Reads the feature- and label-file and duplicates all the
 # images with multiple labels. For example
@@ -88,13 +99,17 @@ def fetchLabels(class_path_file):
 	return np.asarray(labels,dtype='int32')
 
 if __name__ == '__main__':
-	#gammas=[]
-	#gammas.append(0.5)
-	#kernels=[]
-	#for gm in gammas:
-	#	kernels.append(getChi2Kernel(gm))
+	# Set up kernels
+	gammas=[]
+	gammas.append(0.5)
+	kernels=[]
+	for gm in gammas:
+		kernels.append(getChi2Kernel(gm))
 
-	clf = Scikit_SVM('chi')
-	X,y = clf.load_data('standard')
-	print(len(X))
-	print(len(y))
+	# Load data
+	X = np.loadtxt('features.txt',np.float32)
+
+
+	for ker in kernels:
+		svm = Scikit-SVM(ker)
+		svm.train(data,responses)
